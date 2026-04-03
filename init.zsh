24955486446abb7a2066536cf934c7c47686a6d6
Philip M. Gollucci <pgollucci@p6m7g8.com>
2026-04-02 23:36:17 -0400

    feat(profile): add Confluence env vars to profile mod

--- init.zsh
+++ init.zsh
@@ -73,5 +73,5 @@ p6df::modules::confluence::mcp() {
 ######################################################################
 p6df::modules::confluence::profile::mod() {
 
-  p6_return_words 'confluence' "$"
+  p6_return_words 'confluence' '$CONFLUENCE_DOMAIN' '$CONFLUENCE_EMAIL' '$CONFLUENCE_API_TOKEN'
 }
